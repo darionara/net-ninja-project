3d7dfa9a10a992bad9163bd7ddf2b6bf1b685075
Daria Ziobrowska <darianocon@gmail.com>
2023-02-13 20:01:23 +0100

    Pass function handleDelete as props to BlogList

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,6 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
+    /*  If I put (props) as an argument, instead of { blog, title }, I will need to use the below code to 
+        use these props  */
     // const blogs = props.blogs;
     // const title= props.title;
 
@@ -9,6 +11,8 @@ const BlogList = ({ blogs, title }) => {
                 <div className="blog-preview" key={ blog.id }>
                     <h2>{ blog.title }</h2>
                     <p>Written by: { blog.author }</p>
+{/* I am  using the anonymous function not to fire this function on the first render but on the mouse click*/}
+                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
